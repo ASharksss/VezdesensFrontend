249c0513952c9c790e33c06d077f63b739822671
asharksss <alsukurbanalieva@gmail.com>
2024-01-22 12:23:22 +0300

    modal

--- src/components/modal/hideAd.jsx
+++ src/components/modal/hideAd.jsx
@@ -3,12 +3,12 @@ import './modal.css'
 
 const HideAd = ({setCheck, setActive}) => {
 	return (
-		<div className='hide_ad flex column center items-center'>
-			<h1 className='hide_ad-title'>Вы уверены,</h1>
-			<span className='hide_ad-subtitle'>что хотите снять публикацю?</span>
+		<div className='flex column center items-center'>
+			<h1 className='hide_title'>Вы уверены,</h1>
+			<span className='hide_subtitle'>что хотите снять публикацю?</span>
 			<div className="flex center" >
-				<button className='hide_ad-btn hide_ad-yes' onClick={() => setCheck(true)}>Да</button>
-				<button className='hide_ad-btn hide_ad-no' onClick={() => setActive(false)}>Нет</button>
+				<button className='hide_btn hide_ad-yes' onClick={() => setCheck(true)}>Да</button>
+				<button className='hide_btn hide_ad-no' onClick={() => setActive(false)}>Нет</button>
 			</div>
 		</div>
 	);
