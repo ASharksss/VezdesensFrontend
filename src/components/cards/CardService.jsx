361f328373d5ebd3ee90f099fbdcdc845de9c893
asharksss <alsukurbanalieva@gmail.com>
2024-01-22 15:44:12 +0300

    modal

--- src/components/cards/CardService.jsx
+++ src/components/cards/CardService.jsx
@@ -13,12 +13,12 @@ const CardService = ({type, item, setPhone, setActiveModal}) => {
 	return (
 		<div className='card_service'>
 			<img src={`${STATIC_HOST}/${item.previewImageAds[0].name}`} alt={item.title} className='card_service-img'/>
-			<NavLink to={`/card/${item.id}`}>
+			<NavLink to={`/card/${item.id}`} >
 				<div className="card_service-info">
 					<h1 className='card_service-title'>{item.title}</h1>
 					<p className='card_service-wage'>{numberWithSpaces(parseInt(item.price))} ₽</p>
 					<div className='card_service-description'>
-						<pre className='card_description-pre'>{item.description}</pre>
+						<pre className='service_description-pre'>{item.description}</pre>
 					</div>
 				</div>
 			</NavLink>
@@ -28,7 +28,8 @@ const CardService = ({type, item, setPhone, setActiveModal}) => {
 					setActiveModal(true)
 				}}/> : null}
 				{isAuth ? item.user.id !== user.items.id ?
-					<NavLink
+
+					<NavLink className='ml-20'
 						to={`/profile/${item.user.id}?adId=${item.id}&senderId=${user.items.id === item.user.id ? item.user.id : user.items.id}&receiverId=${user.items.id !== item.user.id && item.user.id}#chat-${uuidV4()}`}>
 						<Button classname={'message'} icon={message}/>
 					</NavLink> : null : null}
